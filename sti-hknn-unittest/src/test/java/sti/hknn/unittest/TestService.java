720632a4b816bfc7d3ac7885db6d528500bd4d45
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-01-31 17:54:33 +0100

    fully implemented unit tests

--- sti-hknn-unittest/src/test/java/sti/hknn/unittest/TestService.java
+++ sti-hknn-unittest/src/test/java/sti/hknn/unittest/TestService.java
@@ -1,18 +1,35 @@
 package sti.hknn.unittest;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import sti.hknn.domain.Course;
 import sti.hknn.domain.Student;
+import sti.hknn.service.StiService;
 
 import java.util.ArrayList;
 
+import static sti.hknn.domain.Vault.courseList;
+
 public class TestService {
+    ApplicationContext applicationContext= new ClassPathXmlApplicationContext("classpath:sti-hknn-service.xml");
+    private StiService stiService;
     @Test
     public void studentConstructorTest() {
-        ArrayList<Course> courseList = new ArrayList<>();
         Student student = new Student("firstName", "lastName", "personalId", courseList, "computer");
         Assert.assertEquals("firstName", student.getFirstName());
+    }
+
+    @Before
+    public void Before(){
+        stiService = (StiService) applicationContext.getBean("stiService");
+    }
+    @Test
+    public void studentConstructorTest2(){
+        Student student = stiService.addStudent("firstName", "lastName", "personalId", courseList, "computer");
+        Assert.assertNotNull(student);
     }
 
 }
