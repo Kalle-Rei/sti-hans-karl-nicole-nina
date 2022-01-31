b05611fe0f03657b72e6121fccb4dad0e31e18bc
Karl Reithmeier <kalle_reithmeier@hotmail.com>
2022-01-31 17:31:22 +0100

    added rudimentary unittest; updated gitignore

--- sti-hknn-unittest/src/test/java/sti/hknn/unittest/TestService.java
+++ sti-hknn-unittest/src/test/java/sti/hknn/unittest/TestService.java
@@ -1,4 +1,18 @@
 package sti.hknn.unittest;
 
+import org.junit.Assert;
+import org.junit.Test;
+import sti.hknn.domain.Course;
+import sti.hknn.domain.Student;
+
+import java.util.ArrayList;
+
 public class TestService {
+    @Test
+    public void studentConstructorTest() {
+        ArrayList<Course> courseList = new ArrayList<>();
+        Student student = new Student("firstName", "lastName", "personalId", courseList, "computer");
+        Assert.assertEquals("firstName", student.getFirstName());
+    }
+
 }
